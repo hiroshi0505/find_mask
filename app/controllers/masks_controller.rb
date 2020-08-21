79eca67b7faec20854ffbb945ebaee9ddbc42bee
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-21 18:31:02 +0900

    deviseの導入

--- app/controllers/masks_controller.rb
+++ app/controllers/masks_controller.rb
@@ -1,8 +1,19 @@
 class MasksController < ApplicationController
+  # indexアクションへのリダイレクトを繰り返して無限ループが起こる対策
+  before_action :move_to_index, except: :index
 
   # @masksというインスタンス変数に、masksテーブルのレコードを全て代入
   # この@masksは、ビューファイルで使用
   def index
     @masks = Mask.all
   end
+
+  private
+  # もしユーザーがログインしていなかったらindexアクションにリダイレクトする
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
