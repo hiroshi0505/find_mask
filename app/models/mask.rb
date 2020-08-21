ccb126254d15bcb5972e3b3d7156f93d431a93f0
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-22 00:00:29 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/mask.rb
+++ app/models/mask.rb
@@ -1,4 +1,9 @@
 class Mask < ApplicationRecord
-  has_one_attached :image
-  validates :name, :text, :image, presence: true
+  has_one_attached :image # maskモデルの各レコードは1つの画像ファイルを添付できる
+
+  # (これ必要か？)画像さえあれば、nameとtextが無くても投稿が可能となる
+  validates :name, :text, presence: true, unless: :was_attached?
+  def was_attached?
+    self.image.attached?
+  end
 end
