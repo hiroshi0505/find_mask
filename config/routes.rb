096c8e8f0fc1f38fc2dc0507ebdb2ee685cbaf3a
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-22 14:12:38 +0900

    削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users #ユーザー機能に必要なルーティングを一気に生成してくれるdeviseのメソッド
   # localhost:3000にアクセスした場合、masksコントローラのindexアクションが発動
   root to: "masks#index"
-  resources :masks, only: [:index, :new, :create]
+  resources :masks, only: [:index, :new, :create, :destroy]
 end
