c4c6a431cd11c4b25d12a4980f41bf9c79e51cf0
hiroshi0505 <a1500095@hotmail.co.jp>
2020-08-24 01:32:22 +0900

    comment機能とsearch機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users #ユーザー機能に必要なルーティングを一気に生成してくれるdeviseのメソッド
   # localhost:3000にアクセスした場合、masksコントローラのindexアクションが発動
   root to: "masks#index"
-  resources :masks
+  resources :masks do
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_172605) do
+ActiveRecord::Schema.define(version: 2020_08_23_093017) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2020_08_22_172605) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "mask_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "masks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
